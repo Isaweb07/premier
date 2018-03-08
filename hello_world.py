9938b816129c78908208ddd0545ded03cf9f655f
Isaweb <isa.tic@free.fr>
2018-03-08 10:38:49 +0100

    premier commit python

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+etudiants = [
+    {'nom': 'Dupont', 'promo': 'M1'},
+    {'nom': 'Durand', 'promo': 'M2'},
+    {'nom': 'Dupontel', 'promo': 'M1'},
+    {'nom': 'Antoine', 'promo': 'M1'},
+]
+
+etudiants_m1 = [e['nom'] for e in etudiants if e['promo'] == 'M1']
+etudiants_m2 = [e['nom'] for e in etudiants if e['promo'] == 'M2']
+
+print("Inscrits en M1 :", etudiants_m1)
+print("Inscrits en M2 :", etudiants_m2)
