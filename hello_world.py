d260f208f65d2eed37623a1e027ed0706e700076
Isaweb07 <37176847+Isaweb07@users.noreply.github.com>
2018-03-13 15:14:58 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@ etudiants = [
     {'nom': 'Dupont', 'promo': 'M1'},
     {'nom': 'Durand', 'promo': 'M2'},
     {'nom': 'Dupontel', 'promo': 'M1'},
-    {'nom': 'Antoine', 'promo': 'M1'},
+    {'nom': 'Antoine', 'promo': 'M2'},
 ]
 
 etudiants_m1 = [e['nom'] for e in etudiants if e['promo'] == 'M1']
